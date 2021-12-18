fcfaa957449a814011bc1284d456889418df14bd
Roger Coll <rogercoll@protonmail.com>
2021-12-18 18:39:02 +0100

    feat: working without concurrency

--- src/lehmer.rs
+++ src/lehmer.rs
@@ -1,6 +1,56 @@
-pub fn compute(data: &[u8]) -> u8 {
-    for num in data.iter() {
-        println!("{}", num);
+extern crate num;
+
+use num::{bigint::BigUint, One, Zero};
+
+fn factorial(value: u32) -> BigUint {
+    (2..=value).fold(BigUint::one(), |res, n| res * n)
+}
+
+fn smaller_values(val: u8, data: &[u8]) -> u8 {
+    let mut total = 0;
+    for &num in data {
+        if num < val {
+            total += 1
+        }
     }
-    1
+    total
+}
+
+pub fn compute(data: &[u8]) -> String {
+    // dont need to check last value as last value will have 0 smaller values on the right (0*0!)
+    let mut result: BigUint = Zero::zero();
+    for i in 0..data.len() - 1 {
+        result +=
+            smaller_values(data[i], &data[i + 1..]) * factorial(data.len() as u32 - 1 - i as u32);
+    }
+    result.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use num::bigint::ToBigUint;
+
+    #[test]
+    fn factorial_test() {
+        assert_eq!(1.to_biguint().unwrap(), factorial(0));
+        assert_eq!(1.to_biguint().unwrap(), factorial(1));
+        assert_eq!(2.to_biguint().unwrap(), factorial(2));
+        assert_eq!(40320.to_biguint().unwrap(), factorial(8));
+        assert_eq!(479001600.to_biguint().unwrap(), factorial(12));
+    }
+
+    #[test]
+    fn smaller_values_test() {
+        assert_eq!(2, smaller_values(3, &[1, 2, 3]));
+        assert_eq!(0, smaller_values(1, &[1, 2, 3]));
+        assert_eq!(1, smaller_values(2, &[1, 2, 3]));
+    }
+
+    #[test]
+    fn compute_test() {
+        assert_eq!("5", compute(&[3, 2, 1]));
+        assert_eq!("0", compute(&[1, 2, 3]));
+        assert_eq!("3", compute(&[2, 3, 1]));
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -29,15 +29,18 @@ fn parse_deck(deck: &str) -> Result<Vec<u8>, Box<dyn Error>> {
         return Err("Duplicated cards on the provided deck".into());
     }
     if any_greater_than(&values, DECK_LENGTH as u8) {
-        return Err(format!("Invalid card numbers").into());
+        return Err(format!(
+            "Invalid card numbers, any card can be higher than: {}",
+            DECK_LENGTH
+        )
+        .into());
     }
-    lehmer::compute(&values);
     Ok(values)
 }
 
-pub fn extract_value(deck: &str) -> Result<&str, Box<dyn Error>> {
-    let _cards: Vec<u8> = parse_deck(deck)?;
-    Ok(deck)
+pub fn extract_value(deck: &str) -> Result<String, Box<dyn Error>> {
+    let cards: Vec<u8> = parse_deck(deck)?;
+    Ok(lehmer::compute(&cards))
 }
 
 #[cfg(test)]
@@ -48,11 +51,22 @@ mod tests {
     fn ectract_value_test() {
         assert!(extract_value("1,2,3").is_err());
         assert!(extract_value("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,50,52").is_err());
+        assert_eq!("19162120637135135995112400712879924970218301522662289893153979134230", extract_value("13,20,3,4,5,6,7,8,9,42,11,12,1,14,15,16,17,18,19,2,21,22,23,40,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,24,41,10,43,50,45,46,47,48,49,44,51,52").unwrap());
     }
+
     #[test]
     fn greater_than_test() {
         assert_eq!(false, any_greater_than(&[1, 2, 3], 4));
         assert_eq!(true, any_greater_than(&[1, 2, 3], 2));
         assert_eq!(true, any_greater_than(&[1, 2, 3], 0));
+    }
+
+    #[test]
+    fn has_unique_elements_test() {
+        assert_eq!(false, has_unique_elements(&[1, 2, 3, 1]));
+        assert_eq!(false, has_unique_elements(&[2, 2, 3, 1]));
+        assert_eq!(false, has_unique_elements(&[1, 2, 3, 3]));
+        assert_eq!(false, has_unique_elements(&[1, 2, 3, 3]));
+        assert_eq!(true, has_unique_elements(&[2, 3, 1]));
     }
 }
