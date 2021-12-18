56a66e425592ee1ac8cca2b84318fadc67ff1cbf
Roger Coll <rogercoll@protonmail.com>
2021-12-18 13:34:35 +0100

    test: check any greater

---
+++ src/lehmer.rs
@@ -0,0 +1,6 @@
+pub fn compute(data: &[u8]) -> u8 {
+    for num in data.iter() {
+        println!("{}", num);
+    }
+    1
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+pub mod lehmer;
+
 use std::error::Error;
 
 const DECK_LENGTH: usize = 52;
@@ -6,8 +8,8 @@ fn has_unique_elements(slice: &[u8]) -> bool {
     !(1..slice.len()).any(|i| slice[i..].contains(&slice[i - 1]))
 }
 
-fn greater_than(slice: &[u8], max: u8) -> bool {
-    !(1..slice.len()).any(|i| i > 52)
+fn any_greater_than(slice: &[u8], max: u8) -> bool {
+    slice.iter().any(|&i| i > max)
 }
 
 fn parse_deck(deck: &str) -> Result<Vec<u8>, Box<dyn Error>> {
@@ -26,14 +28,15 @@ fn parse_deck(deck: &str) -> Result<Vec<u8>, Box<dyn Error>> {
     if !has_unique_elements(&values) {
         return Err("Duplicated cards on the provided deck".into());
     }
-    if greater_than(&values, DECK_LENGTH as u8) {
+    if any_greater_than(&values, DECK_LENGTH as u8) {
         return Err(format!("Invalid card numbers").into());
     }
+    lehmer::compute(&values);
     Ok(values)
 }
 
 pub fn extract_value(deck: &str) -> Result<&str, Box<dyn Error>> {
-    let cards: Vec<u8> = parse_deck(deck)?;
+    let _cards: Vec<u8> = parse_deck(deck)?;
     Ok(deck)
 }
 
@@ -45,5 +48,11 @@ mod tests {
     fn ectract_value_test() {
         assert!(extract_value("1,2,3").is_err());
         assert!(extract_value("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,50,52").is_err());
+    }
+    #[test]
+    fn greater_than_test() {
+        assert_eq!(false, any_greater_than(&[1, 2, 3], 4));
+        assert_eq!(true, any_greater_than(&[1, 2, 3], 2));
+        assert_eq!(true, any_greater_than(&[1, 2, 3], 0));
     }
 }
